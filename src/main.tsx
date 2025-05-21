7930df0601f74fefef2ed1c32662b26ef4ed6a1b
Thiago <thiagoamthypc@gmail.com>
2025-05-21 03:06:50 -0300

    arrumando

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
 
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
