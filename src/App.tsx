b783b2f19544b3367aa36c74dbd0700eed1d333b
Thiago <thiagoamthypc@gmail.com>
2025-05-21 02:57:02 -0300

    atualizando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Page from "./components/Page"
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Page />
+      <Routes>
+        <Route path="/" element={<Page />} />
+      </Routes>
     </>
   )
 }
